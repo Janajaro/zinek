d76557cff9e330cc410b472d5612c91153ee3418
Jana Jarošová <janamjaros@seznam.cz>
2026-03-17 14:19:26 +0100

    cms bug fix

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,30 +1,21 @@
 import "./List.css";
-import guestList from "../../data/guests.json";
+import guestsData from "../../data/guests.json";
 import { GuestCard } from "../../components/GuestCard/GuestCard";
 
 export const List = () => {
-
-  const sortedGuests = [...guestList].sort((a, b) =>
+  const sortedGuests = [...guestsData.guests].sort((a, b) =>
     (a.name || "").localeCompare(b.name || "", "cs", { sensitivity: "base" })
   );
 
-    return (
+  return (
+    <>
+      <section className="guests-grid">
+        {sortedGuests.map((item) => (
+          <GuestCard key={item.id} guest={item} />
+        ))}
+      </section>
 
-        <>
-            <section className="guests-grid">
-           
-                { sortedGuests.map((item) => (
-
-                    < GuestCard key={item.id} guest={item}/>
-
-                ))}
-
-            </section>
-
-            <h1 className="guests-heading">... and more coming up!</h1>
-        
-        </>
-
-
-    )
-}
+      <h1 className="guests-heading">... and more coming up!</h1>
+    </>
+  );
+};
